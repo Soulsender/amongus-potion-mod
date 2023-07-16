2c5589cc4af997d3146d126f5a7d2e6d63c931f7
Soulsender <nexushopper3000@gmail.com>
2023-07-16 00:49:05 -0700

    seperated client and main classes

--- src/client/java/name/modid/AmongusPotionClient.java
+++ src/client/java/name/modid/AmongusPotionClient.java
@@ -5,7 +5,7 @@ import name.modid.entity.client.AmongusRedRenderer;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
 
-public class ExampleModClient implements ClientModInitializer {
+public class AmongusPotionClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
 		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
